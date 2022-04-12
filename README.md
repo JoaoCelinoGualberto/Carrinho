e570007b13e73027152ad7c48551c3c9cdcd797d
João Celino Gualberto <joaocg14@gmail.com>
2022-04-12 16:15:43 -0300

    Consumindo API

---
+++ README.md
@@ -0,0 +1,49 @@
+<h1 align="center">
+#img aqui
+</h1>
+
+<h1 align="center">
+#img aqui
+</h1>
+
+## 📝Sobre
+
+O Projeto **Meu Carrinho** é uma interface One Page criada com o intuito de consumir uma API que simula compras em uma loja online trazendo em formato de lista informações sobre os itens e sendo possível adicioná-los em um carrinho, em que pedidos acima de R$ 10.00 o usuário recebe gratuidade no frete.
+___
+
+## 🚀Tecnologias utilizadas
+
+O projeto foi desenvolvido utilizando as seguintes tecnologias:
+
+- [Vue](https://vuejs.org/) 
+- [Flex Box]
+___
+
+## 📦Como baixar e executar o projeto
+```bash
+
+#Clonar o repositório
+$ git clone https://github.com/JoaoCelinoGualberto/MeuCarrinho.git
+
+#Entrar no diretório
+$ cd Carrinho
+
+#Instalar as dependências
+$ npm install
+
+#Inicializar o projeto
+$ npm run serve
+```
+
+## 📦 Demais comandos
+```bash
+
+#Compilar para a produção
+$ npm run build
+
+Lints and fixes files
+$ npm run lint
+```
+___
+
+Desenvolvido por João Celino Gualberto ✅
---
+++ src/components/Card.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <div class="p-3" v-for="item in this.$attrs" :key="item.id">
+      <div class="card-bg--color">
+        <div class="d-flex flex-row align-content-center align-items-center">
+          <img class="img-size bg-white" :src="item.imageUrl" alt="" />
+          <div class="ml-3">
+            <p class="product-name">
+              {{ item.name }}
+            </p>
+            <p class="product-name--dark">
+              R$: {{ item.priceTags[0].value * -1 }}
+            </p>
+          </div>
+        </div>
+        <button v-on:click.stop.prevent="send(item)" class="btn ml-2 mb-2">
+          <img
+            class="car-size"
+            src="../assets/img/adicionar-carrinho-de-compras.svg"
+            alt=""
+          />
+          <span class="it__shelf--text"> Adicionar ao carrinho </span>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  methods: {
+    send: function (product) {
+      this.$emit("carrinho", { payload: product });
+    },
+  },
+};
+</script>
+
+<style scoped>
+.img-size {
+  max-width: 175px;
+}
+.car-size {
+  width: 20px;
+  margin: 0 0 10px 0px;
+}
+
+.card-bg--color {
+  border: 0.1px solid #d1d8e5;
+  border-radius: 5px;
+}
+
+.btn {
+  background: #00fa9a;
+}
+</style>
